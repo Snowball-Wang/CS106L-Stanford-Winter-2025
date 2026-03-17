81386332d21ae9348213308c82055416a7c3bdd5
Jay Wang <jay.wang2@arm.com>
2026-03-17 14:30:12 +0000

    complete assign5

--- cs106l-assignments/assign5/user.cpp
+++ cs106l-assignments/assign5/user.cpp
@@ -63,3 +63,76 @@ void User::set_friend(size_t index, const std::string& name)
  * STUDENT TODO:
  * The definitions for your custom operators and special member functions will go here!
  */
+
+ /**
+  * Overloads << operator to print User object.
+  * @param os ofstream object.
+  * @param user User object.
+  */
+std::ostream&
+operator<<(std::ostream& os, const User& user)
+{
+  os << "User(name=" << user.get_name() << ", ";
+  os << "friends=[";
+  size_t size = user.size();
+  for (size_t i = 0; i < size; ++i) {
+    if (i < size - 1)
+      os << user._friends[i] << ", ";
+    else
+      os << user._friends[i];
+  }
+  os << "])";
+  return os;
+}
+
+/* User-defined destructor */
+User::~User()
+{
+  delete [] _friends;
+  _friends = nullptr;
+}
+
+/* User-defined copy constructor */
+User::User(const User& user)
+: _name(user._name), _friends(new std::string[user._capacity]), _size(user._size), _capacity(user._capacity)
+{
+  if (&user != this) {
+    for (size_t i = 0; i < user._size; ++i)
+      _friends[i] = user._friends[i];
+  }
+}
+
+/* User-defined copy assignment operator */
+User& User::operator=(const User& user)
+{
+  if (&user != this) {
+    _name = user._name;
+    _size = user._size;
+    _capacity = user._capacity;
+    std::string* newFriends = new std::string[_capacity];
+    for (size_t i = 0; i < _size; ++i)
+      newFriends[i] = user._friends[i];
+    delete [] _friends;
+    _friends = newFriends;
+  }
+  return *this;
+}
+
+/* Overload += operator */
+User& User::operator+=(User& other)
+{
+  if (&other != this) {
+    this->add_friend(other.get_name());
+    other.add_friend(this->get_name());
+  }
+  return *this;
+}
+
+/* Overload < operator */
+bool User::operator<(const User& other) const
+{
+  if (this->_name < other._name)
+    return true;
+  else
+    return false;
+}
--- cs106l-assignments/assign5/user.h
+++ cs106l-assignments/assign5/user.h
@@ -19,6 +19,21 @@ public:
    * STUDENT TODO:
    * Your custom operators and special member functions will go here!
    */
+  /* Overload << operator */
+  friend std::ostream& operator<<(std::ostream& os, const User& user);
+  /* User-defined destructor */
+  ~User();
+  /* User-defined copy constructor */
+  User(const User& user);
+  /* User-defined copy assignment operator */
+  User& operator=(const User& user);
+  /* Prevent the User class from being move constructed/assigned */
+  User(const User&& user) = delete;
+  User& operator=(User&& user) = delete;
+  /* Overload += operator */
+  User& operator+=(User& other);
+  /* Overload < operator */
+  bool operator<(const User& other) const;
 
 private:
   std::string _name;
